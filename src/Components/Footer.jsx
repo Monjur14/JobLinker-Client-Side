298affa1fe472953296ad8db507eb948798653cf
Monjur Hossen <14monjurhossen@gmail.com>
2024-06-25 17:56:54 +0600

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       
 
 <footer className="bg-[rgb(18,21,56)]">
-    <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+    <div className="contain p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
               <a className="flex items-center">
