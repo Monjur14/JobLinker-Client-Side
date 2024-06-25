8b9c1db18fa0e064749e40d03acbec337ad766c3
Monjur Hossen <14monjurhossen@gmail.com>
2024-06-25 17:52:58 +0600

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
     <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" className="flex items-center">
-                  <img src="/public/logo.png" className="h-8 me-3" alt="FlowBite Logo" />
+              <a className="flex items-center">
+                  <img src="/logo.png" className="h-8 me-3" alt="JonLinker Logo" />
               </a>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -51,7 +51,7 @@ const Footer = () => {
       </div>
       <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© <a href="https://flowbite.com/" className="hover:underline">JobLinker</a>. All Rights Reserved.
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© <a href="#" className="hover:underline">JobLinker</a>. All Rights Reserved.
           </span>
           <div className="flex mt-4 sm:justify-center sm:mt-0">
               <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
